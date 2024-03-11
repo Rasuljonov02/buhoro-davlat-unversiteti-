7a97b6830782f5c99b29d2290736f0821424a3bd
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-03-11 16:52:30 +0500

    555

--- src/components/Hedr.vue
+++ src/components/Hedr.vue
@@ -33,16 +33,15 @@ export default {
 <template>
 	<!-- <div class="grid place-items-center bg-slate-400 min-w-[300px]"></div> -->
 
-	<header class="fixed w-full">
-		<nav class="bg-white items-center flex justify-between border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+	<header>
+		<nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+			<div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
 				<a to="/" class="flex items-center">
 					<img
     src="@/img/logo.jpg"
     class="mr-3 h-6 sm:h-9"
     alt="Flowbite Logo"
 /></a>
-			<div class="flex flex-wrap  items-center mx-auto max-w-screen-xl">
-			
 				<div class="flex items-center lg:order-2">
 					<button
 					id="btn"
