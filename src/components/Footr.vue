fcb19aba89cc0604e83b9359c0c376355de2bd27
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-03-11 14:41:50 +0500

    m8

--- src/components/Footr.vue
+++ src/components/Footr.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="min-w-[390px] bg-[#26282c] text-white mt-10">
-    <div class="">
-      <!-- Main heading -->
-      <h2 class="font-Arial text-[35px] font-extrabold">Karakul Lyceum</h2>
-      <!-- Sub heading -->
-      <h3 class="font-Arial text-[30px] " >Buxoro davlat universiteti qoshidagi</h3>
-      <!-- Paragraph -->
-      <p class="font-Arial text-[25px] ">Qorako`l akademik litseyi</p>
+  <div class="min-w-[390px] bg-[#26282c] p-5 text-white mt-10 pl-[50px] pr-[50px]">
+    <div class="flex flex-col gap-2 ">
+      <h2 class="font-Arial text-[40px] font-extrabold">Karakul Lyceum</h2>
+      <h3 class="font-Arial text-[28px] font-[500]">Buxoro davlat universiteti qoshidagi</h3>
+      <p class="font-Arial text-[25px] font-[300]">Qorako`l akademik litseyi</p>
+      <div class="mt-[60px]">
+        <!-- Image element -->
+        <div class="flex flex-col gap-5 ">
+          <p  class="flex ite\ gap-4">
+            <img class="w-[35px]"  src="@/img/local.png" alt="">
+            Buxoro viloyati, Qorako`l tumani
+          </p>
+          <p class="flex ite\ gap-4">
+        <img class="w-[35px]" src="@/img/call.png" alt="Call Image">
+        +(998) 90-000-00-00
+          </p>
+          <p class="flex ite\ gap-4">
+            <img  class="w-[35px]"  src="@/img/email.png" alt="">example.mail.ru
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  // JavaScript logic goes here
+
 </script>
 
 <style>
-  /* CSS styles go here */
 </style>
