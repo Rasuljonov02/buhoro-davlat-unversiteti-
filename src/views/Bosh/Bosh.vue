761c81a78bc04cc437041b9c0cb9126ba5ef11e9
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-03-12 01:25:04 +0500

    se

--- src/views/Bosh/Bosh.vue
+++ src/views/Bosh/Bosh.vue
@@ -3,12 +3,13 @@
   class="pl-[50px] pr-[50px]">
  <h1>bosh Component</h1>
   <div class="min-w-[400px] flex items-center flex-col">
-    <h1 class="font-Arial text-[28px] font-[500]">Bizning yo'nalishlar</h1>
-    <h2 class="font-Arial text-[40px] font-bold ">Ushbu yo'nalishlarda maqola berishingiz mumkin</h2>
+    <div class="min-w-[400px] ">
+      <h1 class="font-Arial text-[28px] lg:text-[18px] font-[500]">Bizning yo'nalishlar</h1>
+    <h2 class="font-Arial lg:text-[25px] text-[40px] font-bold ">Ushbu yo'nalishlarda maqola berishingiz mumkin</h2>
+    </div>
     <!-- slider  -->
-    <div  class="swiffy-slider mt-5 grid place-items-center min-w-[400px]  slider-item-show3 slider-item-reveal slider-nav-dark slider-nav-outside-expand">
-      <div class="swiffy-slider min-w-[400px]  slider-item-show4 slider-item-show2-sm slider-item-reveal slider-nav-caret slider-nav-touch slider-nav-autoplay slider-nav-autopause slider-indicators-dark slider-indicators-highlight slider-indicators-sm slider-nav-animation-scale">
-    <ul class="slider-container ">
+    <div  class="swiffy-slider mt-5 grid place-items-center min-w-[300px]  slider-item-show3 slider-item-reveal slider-nav-dark slider-nav-outside-expand">
+      <div class="swiffy-slider slider-item-show4 slider-item-snapstart slider-nav-round slider-nav-dark slider-nav-sm slider-nav-autoplay slider-nav-autopause slider-indicators-dark slider-indicators-highlight slider-indicators-sm slider-nav-animation-zoomout slider-nav-animation-fast" data-slider-nav-autoplay-interval="2000">    <ul class="slider-container ">
         <li>
             <div id="slide1"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
         </li>
