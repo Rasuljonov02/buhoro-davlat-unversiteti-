b08ad14cd60b142f19caaa2f4e4eac65b083983a
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-03-12 01:57:01 +0500

    as

--- src/views/Bosh/Bosh.vue
+++ src/views/Bosh/Bosh.vue
@@ -4,38 +4,38 @@
  <h1>bosh Component</h1>
   <div class="min-w-[400px] flex items-center flex-col">
     <div class="min-w-[400px] ">
-      <h1 class="font-Arial text-[28px] lg:text-[18px] font-[500]">Bizning yo'nalishlar</h1>
-    <h2 class="font-Arial lg:text-[25px] text-[40px] font-bold ">Ushbu yo'nalishlarda maqola berishingiz mumkin</h2>
+      <h1 class="font-Arial text-[28px] sm:text-[18px] font-[500]">Bizning yo'nalishlar</h1>
+    <h2 class="font-Arial sm:text-[25px] text-[40px] font-bold ">Ushbu yo'nalishlarda maqola berishingiz mumkin</h2>
     </div>
     <!-- slider  -->
-    <div  class="swiffy-slider mt-5 grid place-items-center min-w-[300px]  slider-item-show3 slider-item-reveal slider-nav-dark slider-nav-outside-expand">
-      <div class="swiffy-slider slider-item-show4 slider-item-snapstart slider-nav-round slider-nav-dark slider-nav-sm slider-nav-autoplay slider-nav-autopause slider-indicators-dark slider-indicators-highlight slider-indicators-sm slider-nav-animation-zoomout slider-nav-animation-fast" data-slider-nav-autoplay-interval="2000">    <ul class="slider-container ">
-        <li>
-            <div id="slide1"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
+    <div class="swiffy-slider mt-5 flex items-center  w-[150px] sm:min-w-[500px] slider-item-show4 slider-item-snapstart slider-nav-round slider-nav-dark slider-nav-sm slider-nav-autoplay slider-nav-autopause slider-indicators-dark slider-indicators-highlight slider-indicators-sm slider-nav-animation-zoomout slider-nav-animation-fast" data-slider-nav-autoplay-interval="2000">
+    <ul class="slider-container" style="">
+        <li class="slide-visible">
+            <div id="slide1"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
+        </li>
+        <li class="slide-visible">
+            <div id="slide2"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
+        </li>
+        <li class="slide-visible">
+            <div id="slide3"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
+        </li>
+        <li class="slide-visible">
+            <div id="slide4"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
         </li>
         <li>
-            <div id="slide2"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
+            <div id="slide5"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
         </li>
         <li>
-            <div id="slide3"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
+            <div id="slide6"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
         </li>
         <li>
-            <div id="slide4"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
+            <div id="slide7"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
         </li>
         <li>
-            <div id="slide5"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
+            <div id="slide8"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
         </li>
         <li>
-            <div id="slide6"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
-        </li>
-        <li>
-            <div id="slide7"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
-        </li>
-        <li>
-            <div id="slide8"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
-        </li>
-        <li>
-            <div id="slide9"><img class="w-[150px]" src="@/img/call.png" alt=""> </div>
+            <div id="slide9"><img class="sm:w-[150px]" src="@/img/call.png" alt=""></div>
         </li>
     </ul>
 
@@ -44,16 +44,15 @@
 
     <ul class="slider-indicators">
         <li class=""></li>
-        <li class=""></li>
-        <li></li>
-        <li class=""></li>
-        <li></li>
-        <li class=""></li>
-        <li></li>
         <li class="active"></li>
         <li class=""></li>
+        <li class=""></li>
+        <li></li>
+        <li class=""></li>
+        <li class=""></li>
+        <li class=""></li>
+        <li class=""></li>
     </ul>
-</div>
 </div>
   </div>
 </div>
